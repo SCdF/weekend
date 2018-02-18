acb3d54961c891b38dde40b966c2c80cc5f3878a
Stefan du Fresne <stefan@sdufresne.info>
2018-02-18 14:40:37 +0000

    Chapter 4: A Red Sphere

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,48 @@ use ray::Ray;
 mod vec3;
 mod ray;
 
+fn hit_sphere(center: Vec3, radius: f32, r: &Ray) -> bool {
+    let oc = r.origin - center;
+    let a = Vec3::dot(&r.direction, &r.direction);
+    let b = 2.0 * Vec3::dot(&oc, &r.direction);
+    let c = Vec3::dot(&oc, &oc) - (radius * radius);
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant > 0.0
+}
+
 fn color(ray: &Ray) -> Vec3 {
-    let unit_direction = ray.direction.unit_vector();
-    let t = 0.5 * (unit_direction.y + 1.0);
-
-    // eprintln!("{} {}", unit_direction.y, t);
-
-    let white = Vec3 {
-        x: 1.0,
-        y: 1.0,
-        z: 1.0,
+    let sphere_loc = Vec3 {
+        x: 0.0,
+        y: 0.0,
+        z: -1.0,
     };
-    let blue = Vec3 {
-        x: 0.5,
-        y: 0.7,
-        z: 1.0,
-    };
+    let sphere_radius = 0.5;
 
-    (1.0 - t) * white + t * blue
+    if hit_sphere(sphere_loc, sphere_radius, ray) {
+        Vec3 {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+        }
+    } else {
+        // Sky
+        let unit_direction = ray.direction.unit_vector();
+        let t = 0.5 * (unit_direction.y + 1.0);
+
+        let white = Vec3 {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        };
+        let blue = Vec3 {
+            x: 0.5,
+            y: 0.7,
+            z: 1.0,
+        };
+
+        (1.0 - t) * white + t * blue
+    }
 }
 
 fn main() {
